08fe2698861941e860d1ce96d172f765771dfdee
Henrique Silva <henrique.silva@hbsis.com.br>
2020-01-14 10:55:08 -0300

    Movendo o repositório para outra pasta

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
-const LikesController = require('./controllers/LikesController');
+const LikeController = require('./controllers/LikeController');
 
 const routes = express.Router();
 
 routes.post('/devs', DevController.store);
-routes.post('/devs/:devId/likes', LikesController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
 
 module.exports = routes;
