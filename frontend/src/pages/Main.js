36a7774df9598ee25eba22abdf91d9f951c1723d
Henrique Silva <henrique.silva@hbsis.com.br>
2020-01-15 16:59:21 -0300

    Finalizado a parte reactJS

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Main () {
-    return (
-        <h1>Hello Word</h1>
-    )
+import "./Main.css";
+
+import logo from "../assets/logo.svg";
+import like from "../assets/like.svg";
+import dislike from "../assets/dislike.svg";
+import api from "../services/api";
+
+export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loaderUsers() {
+      const response = await api.get("/devs", {
+        headers: { user: match.params.id }
+      });
+      setUsers(response.data);
+    }
+    loaderUsers();
+  }, [match.params.id]);
+
+  async function handleLike(id) {
+    await api.post(`devs/${id}/likes`, null, {
+      headers: { user: match.params.id }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  async function handleDislike(id) {
+    await api.post(`devs/${id}/dislikes`, null, {
+      headers: { user: match.params.id }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  return (
+    <div className="main-container">
+      <Link to="/">
+        <img src={logo} alt="Tindev" />
+      </Link>
+      {users.length > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt="Avatar" />
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={dislike} alt="Dislike" />
+                </button>
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  <img src={like} alt="Like" />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        // eslint-disable-next-line jsx-a11y/accessible-emoji
+        <div className="empty">Acabou 😢</div>
+      )}
+    </div>
+  );
 }
